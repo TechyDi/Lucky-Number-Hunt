243c9b1d74dedd4482779571e1f29a812ba5c507
TechyDi <dkd1210202gmail.com>
2024-09-10 19:43:29 +0530

    Simply completed and need optimization

--- game.java
+++ game.java
@@ -3,25 +3,32 @@ import java.util.Random;
 public class game {
 
     public static void main(String[] args) {
-        System.out.println("\nWELCOME TO LUCKY NUMBER HUNT\n");
         Random rn = new Random();
-
         int randomNum = rn.nextInt(100);
-        //System.out.println(randomNum); // Checking the Random number
+        System.out.println(randomNum); // Checking the Random number
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("The Random Number is Generated & It's time to Check Your Luck\nEnter Your Number:- ");
-        int num1 = sc.nextInt();
+        System.out.println("\nWELCOME TO LUCKY NUMBER HUNT");
+        System.out.println("Condition:-1. Choose your Number between 0 and 100\n");
+        System.out.print("Enter Your Name: ");
+            String name = sc.nextLine();
 
-        if(num1<randomNum){
-            System.out.println("It is too Small.");
-        }else if(num1>randomNum){
-            System.out.println("It is too Big");
-        }else if (num1==randomNum){
-            System.out.println("Yahooo! You Guessed Right");
-        }else {
-            System.out.println("Invalid Choice");
-        }
+        System.out.println("\nHii "+ name +" Wlcome to the game.");
+            System.out.print("\nThe Random Number is Generated & It's time to Check Your Luck\nEnter Your Number:- ");
+
+                while (true) {
+                    int num = sc.nextInt();
+                        if(randomNum==num){
+                            System.out.println("Yahoo! Congratulation, You win.");
+                            break;
+                        }else if (randomNum>num){
+                            System.out.println("To Small.");
+                        }else if(randomNum<num){
+                            System.out.println("Too Big.");
+                        }
+                    
+                }
+
 
         }
     }
